bc8de356cb9482895511b33157674f454ca39e05
Felipe Grigolli Clemente <felipe.grigolli@hotmail.com>
2024-04-13 22:52:50 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,59 @@
+var nameElement = document.getElementById('name');
+var jobElement = document.getElementById('job');
+
+var nameText = nameElement.innerText;
+var jobText = jobElement.innerText;
+
+nameElement.innerText = '';
+jobElement.innerText = '';
+
+function typeWriter(text, element, callback) {
+  var i = 0;
+  var speed = 100;
+
+  function type() {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(type, speed);
+    } else {
+      if (typeof callback === 'function') callback();
+    }
+  }
+
+  type();
+}
+
+typeWriter(nameText, nameElement, function() {
+  typeWriter(jobText, jobElement);
+});
+
+
+window.onload = function() {
+  const texto = document.getElementById('paragrafo');
+  const imagem = document.querySelector('.Programador');
+  
+  texto.classList.add('visible');
+  imagem.classList.add('visible');
+};
+
+
+var lastScrollTop = 0;
+
+// Adicione um evento de rolagem à janela
+window.addEventListener('scroll', function() {
+    // Obtenha a posição atual do scroll
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Verifique se o scroll está se movendo para baixo e se já passou do cabeçalho
+    if (currentScroll > lastScrollTop && currentScroll > 100) {
+        // Role para baixo e esconda o cabeçalho
+        document.querySelector('header').classList.add('hide');
+    } else {
+        // Role para cima e mostre o cabeçalho novamente
+        document.querySelector('header').classList.remove('hide');
+    }
+
+    // Atualize a posição do scroll
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
